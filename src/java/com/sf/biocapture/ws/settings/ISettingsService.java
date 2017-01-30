76ed542076d4ab0c4f999f890a6bef1b14bd7c09
jintao <nmadu@seamfix.com>
2017-01-30 13:45:55 +0100

    modified new settings endpoint path #DK-3

--- src/java/com/sf/biocapture/ws/settings/ISettingsService.java
+++ src/java/com/sf/biocapture/ws/settings/ISettingsService.java
@@ -1,9 +1,9 @@
 package com.sf.biocapture.ws.settings;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,8 +16,8 @@ import javax.ws.rs.core.Response;
  */
 public interface ISettingsService {
 	@POST
-	@Path("/")
+	@Path("/global")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response settings(@PathParam("tag") String tag, @PathParam("mac") String mac);
+	public Response settings(@FormParam("tag") String tag, @FormParam("mac") String mac);
 }
