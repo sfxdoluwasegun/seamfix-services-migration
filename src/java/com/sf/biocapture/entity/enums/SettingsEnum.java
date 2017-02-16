91b114d99bd6f674af53a4789a7af4fb636bd9cf
Marcel Ugwu <mugwu@seamfix.com>
2017-02-16 20:26:38 +0100

    Defined new Endpoint for Client Configuration (Settings)

--- src/java/com/sf/biocapture/entity/enums/SettingsEnum.java
+++ src/java/com/sf/biocapture/entity/enums/SettingsEnum.java
@@ -30,27 +30,6 @@ public enum SettingsEnum {
     MINIMUM_ACCEPTABLE_CHARACTER("MINIMUM-ACCEPTABLE-CHARACTER", "2", "This is the minimum number of allowed characters for name field during foreigner's registration"),
 
     //DYNAMIC BIOSMART SETTINGS
-    VALIDATE_MSISDN_NM("VALIDATE-MSISDN-NM", "true", "Determines whether client will do backend validation for msisdn for New Reg (MSISDN) use case"),
-    PUK_MANDATORY_NM("PUK-MANDATORY-NM", "false", "Determines whether puk is mandatory for New Reg (MSISDN) use case"),
-    VALIDATE_SERIAL_NS("VALIDATE-SERIAL-NS", "true", "Determines whether client will do backend validation for sim serial for New Reg (SIM SERIAL) use case"),
-    PUK_MANDATORY_NS("PUK-MANDATORY-NS", "false", "Determines whether puk is mandatory for New Reg (SIM SERIAL) use case"),
-    VALIDATE_NMS("VALIDATE-NMS", "true", "Determines whether client will do backend validation for msisdn and serial for New Reg (MSISDN+SERIAL) use case"),
-    NMS_PUK_MANDATORY("NMS-PUK-MANDATORY", "false", "Determines whether puk is mandatory for New Reg (MSISSN+SERIAL) use case"),
-    
-    MSISDN_MIN_LENGTH("MSISDN-MIN-LENGTH", "11", "MSISDN minimum length"),
-    MSISDN_MAX_LENGTH("MSISDN-MAX-LENGTH", "11", "MSISDN maximum length"),
-    SERIAL_MIN_LENGTH("SERIAL-MIN-LENGTH", "20", "SERIAL minimum length"),
-    SERIAL_MAX_LENGTH("SERIAL-MAX-LENGTH", "20", "SERIAL maximum length"),
-    PUK_MIN_LENGTH("PUK-MIN-LENGTH", "8", "PUK minimum length"),
-    PUK_MAX_LENGTH("PUK-MAX-LENGTH", "8", "PUK maximum length"),
-    
-    //Biometric Validation
-    PORTRAIT_MANDATORY("PORTRAIT-MANDATORY", "true", "Determines whether portrait capture is mandatory"),
-    VALIDATE_PORTRAIT("VALIDATE-PORTRAIT", "true", "Determines whether portrait validation is required"),
-    FINGERPRINT_MANDATORY("FINGERPRINT-MANDATORY", "true", "Determines whether fingerprint capture is mandatory"),
-    VALIDATE_FINGERPRINT("VALIDATE-FINGERPRINT", "true", "Determines whether fingerprint validation is required"),
-    REQUIRED_FINGERPRINT_TYPES("REQUIRED-FINGERPRINT-TYPES", "RT, RI, RM, RR, RP, LT, LI, LM, LR, LP", "This is a comma separated values of available or required fingerprint types to be captured on the client (R and L stands for Right and Left respectively. T, I, M, R and P stands for Thumb, Index, Middle, Ring and Pinky fingers respectively)"),
-    
     NOTIFICATION_CHECKER_INTERVAL("NOTIFICATION-CHECKER-INTERVAL", "60", "Time interval, in seconds, between which client checks for notifications from server"),
     AGENT_BIOSYNC_INTERVAL("AGENT-BIOSYNC-INTERVAL", "60", "Time interval, in seconds, between which client sends available agents' biometrics to server"),
     AUDIT_SYNC_INTERVAL("AUDIT-SYNC-INTERVAL", "1800", "Time interval, in seconds, between which client sends available client activity logs to HTTP server"),
@@ -61,6 +40,7 @@ public enum SettingsEnum {
     HARMONIZER_INTERVAL("HARMONIZER-INTERVAL", "120", "Time interval, in seconds, between which client checks for status of each registration using unique ID"),
     SETTINGS_INTERVAL("SETTINGS-INTERVAL", "300", "Time interval, in seconds, between which client checks server for settings"),
     OTA_INTERVAL("OTA-INTERVAL", "300", "Time interval, in seconds, between which client checks for new application update"),
+    CLIENT_SETTING_INTERVAL("CLIENT-SETTING-INTERVAL", "300", "Time interval, in seconds, between which client checks for new client settings configuration"),
     
     BLACKLIST_CHECKER_INTERVAL("BLACKLIST-CHECKER-INTERVAL", "600", "Time interval, in seconds, between which client checks server for kit and agent blacklist status");
 
